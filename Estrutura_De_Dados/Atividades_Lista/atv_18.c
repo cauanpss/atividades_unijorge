287a7522ccd8fd95f86f80da8b463eacf8a6f8bf
unknown <cauanp.santos@yahoo.com.br>
2026-03-19 18:09:00 -0300

    atv_18 concluida

---
+++ Estrutura_De_Dados/Atividades_Lista/atv_18.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+int numero = 2 ;
+
+int main(){
+    for(int i = 0; i <= 10; i++){
+        int numero_atual = numero * i;
+        printf("%d x %d = %d \n",numero, i, numero_atual);
+    }
+    
+    
+    return 1;
+}
