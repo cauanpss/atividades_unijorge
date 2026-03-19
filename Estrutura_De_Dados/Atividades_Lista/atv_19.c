2a520ee5128476ff4ae2f6ce2d4df2026cdc6d09
unknown <cauanp.santos@yahoo.com.br>
2026-03-19 18:14:25 -0300

    atv_19 concluida

---
+++ Estrutura_De_Dados/Atividades_Lista/atv_19.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+int numeros[5] = {};
+int positivos = 0;
+
+
+int main(){
+    printf("Digite 5 números: \n");
+    for(int i = 0; i < 5; i++){
+        scanf("%d", &numeros[i]);
+        
+        if(numeros[i] > 0){
+            positivos += 1;    
+        }
+    }
+    printf("O total de números positivos é: %d", positivos);
+    
+    return 1;
+}
