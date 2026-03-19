74af443f601d17885d0b79d209548ac980f39e42
unknown <cauanp.santos@yahoo.com.br>
2026-03-19 18:31:35 -0300

    atv_21 concluida

---
+++ Estrutura_De_Dados/Atividades_Lista/atv_21.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+int n1;
+int n2;
+int produto;
+
+
+int main(){
+    printf("Digite dois números:\n");
+    scanf("%d \n %d", &n1, &n2);
+
+    for( int i = 0; i < n2; i++){
+        produto += n1;
+    }
+    printf("%d", produto);
+
+    return 1;
+}
