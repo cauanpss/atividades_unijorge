409b76434d9e3645f406d6967a7288b89c195293
unknown <cauanp.santos@yahoo.com.br>
2026-03-20 12:26:06 -0300

    att

---
+++ Estrutura_De_Dados/Atividades_Lista/atv_23.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+float maior_altura = 0;
+float menor_altura = 0;
+int homens = 0;
+int mulheres = 0;
+float total_altura_mulheres;
+float percentual_abaixo_1_68 = 0;
+
+int main(){
+
+    // Digite 1 para homem, 2 para mulher:
+    //     armazene homem
+    //     armazene mulher
+    // Digite a sua altura:
+    //     se maior que maioraltura armazene
+    //     se menor que menoraltura armazene
+        
+    // float media_altura_mulheres = total_altura_mulheres / mulheres;
+    // mostrar media altura mulheres
+    
+    return 1;
+}
