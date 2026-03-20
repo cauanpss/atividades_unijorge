3a3a226df9b99ea6a7e4ad25d4a8dfbb27a2466d
unknown <cauanp.santos@yahoo.com.br>
2026-03-20 17:31:54 -0300

    atv_24 concluída

---
+++ Estrutura_De_Dados/Atividades_Lista/atv_24.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+int main(){
+    for(float i = 50, n = 1; i >= 1 && n <= 50 ; i--, n++){
+        float s = i / n ; 
+        printf("%.2f\n", s);
+    }
+
+    return 1;
+}
