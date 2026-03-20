70ff5b7e665bbbabfee87b1c83422cc8128caa50
unknown <cauanp.santos@yahoo.com.br>
2026-03-19 22:49:19 -0300

    atv_22 concluída

---
+++ Estrutura_De_Dados/Atividades_Lista/atv_22.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+int n1;
+
+int tabuada(int numero) {
+        for(int i = 1; i <= 10; i++){
+            int resultado = numero * i;
+            printf("%d x %d = %d \n", numero, i, resultado);        
+        }
+    }
+
+int main(){
+    printf("Digite um número para saber a tabuada deste.\n");
+    scanf("%d", &n1);
+    tabuada(n1);
+    
+    return 1;
+}
