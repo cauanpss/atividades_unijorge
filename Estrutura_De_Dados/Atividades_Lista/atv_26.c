1733a9459af33576622ff0c28813e7b830c33347
unknown <cauanp.santos@yahoo.com.br>
2026-03-26 15:41:26 -0300

    att

---
+++ Estrutura_De_Dados/Atividades_Lista/atv_26.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+int n;
+int inicial = 1;
+
+int main(){
+        printf("insira um valor: \n");
+        scanf("%d", &n);
+        
+        for(int h = 0; h < n; h++){
+            printf("%n");
+        
+        }
+        
+        
+    return 1;
+}
