c078027ea3663e60878893a16e17c30967d6c2b6
unknown <cauanp.santos@yahoo.com.br>
2026-08-14 11:40:56 -0300

    Atividade01 concluida

--- POO/untitled/src/atividade01/Atividade01.java
+++ POO/untitled/src/atividade01/Atividade01.java
@@ -1,9 +1,43 @@
 package atividade01;
 
+import java.util.Scanner;
+
 public class Atividade01 {
-    public static void main(String args){
+    public static void main(String[] args){
 
+        Scanner sc = new Scanner(System.in);
+        int numero1, numero2, limiteSuperior, limiteInferior, somaInteiros, totalPares, totalImpares;
 
+        somaInteiros = 0;
+        totalImpares = 0;
+        totalPares = 0;
 
+        System.out.println("Digite um número: ");
+        numero1 = sc.nextInt();
+        System.out.println("Digite um segundo número: ");
+        numero2 = sc.nextInt();
+
+        if(numero1 > numero2){
+            limiteSuperior = numero1;
+            limiteInferior = numero2;
+        }else{
+            limiteSuperior = numero2;
+            limiteInferior = numero1;
+        }
+
+        for(int i = limiteInferior; i <= limiteSuperior ; i++){
+
+            if(i%2 == 0){
+                System.out.println("O número " + i + " é par.");
+                totalPares += 1;
+            }else{
+                System.out.println("O número " + i + " é impar.");
+                totalImpares += 1;
+            }
+
+            somaInteiros += i;
+        }
+
+        System.out.println("O total da soma enter o intervalo de " + limiteInferior + " até " + limiteSuperior + " é de: " + somaInteiros + ".");
     }
 }
