c810a07db9091a58085d640adf165394e36340e0
unknown <cauanp.santos@yahoo.com.br>
2026-08-26 16:49:15 -0300

    att all files - feat: av3_AOO

--- Analise_orientada_a_objeto/exercicioPratico01/project/src/contaBancaria/ContaBancaria.java
+++ Analise_orientada_a_objeto/exercicioPratico01/project/src/contaBancaria/ContaBancaria.java
@@ -1,4 +1,21 @@
 package contaBancaria;
 
 public class ContaBancaria {
+
+    private String titular;
+    private double saldo;
+    private int numeroConta;
+    private static int contadorContas = 0;
+
+    {
+        System.out.println("Criando nova conta...");
+    }
+
+    public ContaBancaria (String titular, double saldoInicial){
+        this.titular = titular;
+        this.saldo = saldoInicial;
+        this.numeroConta = ++contadorContas;
+    }
+
+
 }
